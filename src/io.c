f58b5ba15bcbf44644e0b361c0a1b51f601c38f8
samloeschen <samloeschen@gmail.com>
2020-03-30 01:44:41 -0400

    fix unused int warning in io.c

--- src/io.c
+++ src/io.c
@@ -15,7 +15,7 @@ int readAll(char *path, char **out) {
     *out = calloc(size + 1, sizeof(char));
     fseek(filePtr, 0L, SEEK_SET);
 
-    size_t newLen = fread(*out, sizeof(char), size, filePtr);
+    fread(*out, sizeof(char), size, filePtr);
     fclose(filePtr);
 
     return 0;
